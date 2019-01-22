de8362f6e0e67d4dacee13054d33359e80adc0ad
Thiago Veloso <thiago.veloso@LIS-MBP-0808.lan>
2019-01-22 17:01:48 +0000

    all

--- fujtsuProj/urls.py
+++ fujtsuProj/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from fujtsuProj.requirementTwo import views
+from requirementTwo import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
