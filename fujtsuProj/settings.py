5ea97101e42ff4678ff6a831f99e58945407b759
Thiago Veloso <thiago.veloso@LIS-MBP-0808.lan>
2019-01-22 16:51:28 +0000

    all

--- fujtsuProj/settings.py
+++ fujtsuProj/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'fujtsuProj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'requirementTwo/fujtsuProj/requirementTwo/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
